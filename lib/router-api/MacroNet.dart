b3db51a54aa4eaa0d245d60cfa95474ccbc4bad9
Alex Tarana <tarana.alex@gmail.com>
2021-02-05 14:52:10 +0100

    MacroNet.dart added

---
+++ lib/router-api/MacroNet.dart
@@ -0,0 +1,37 @@
+class MacroNet {
+  static const String MACRO_NET_WORK_TYPE_EX_NOSERVICE = "0";
+  static const String MACRO_NET_WORK_TYPE_EX_GSM = "1";
+  static const String MACRO_NET_WORK_TYPE_EX_GPRS = "2";
+  static const String MACRO_NET_WORK_TYPE_EX_EDGE = "3";
+  static const String MACRO_NET_WORK_TYPE_EX_IS95A = "21";
+  static const String MACRO_NET_WORK_TYPE_EX_IS95B = "22";
+  static const String MACRO_NET_WORK_TYPE_EX_CDMA_1X = "23";
+  static const String MACRO_NET_WORK_TYPE_EX_EVDO_REV_0 = "24";
+  static const String MACRO_NET_WORK_TYPE_EX_EVDO_REV_A = "25";
+  static const String MACRO_NET_WORK_TYPE_EX_EVDO_REV_B = "26";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_CDMA_1X = "27";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EVDO_REV_0 = "28";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EVDO_REV_A = "29";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EVDO_REV_B = "30";
+  static const String MACRO_NET_WORK_TYPE_EX_EHRPD_REL_0 = "31";
+  static const String MACRO_NET_WORK_TYPE_EX_EHRPD_REL_A = "32";
+  static const String MACRO_NET_WORK_TYPE_EX_EHRPD_REL_B = "33";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EHRPD_REL_0 = "34";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EHRPD_REL_A = "35";
+  static const String MACRO_NET_WORK_TYPE_EX_HYBRID_EHRPD_REL_B = "36";
+  static const String MACRO_NET_WORK_TYPE_EX_WCDMA = "41";
+  static const String MACRO_NET_WORK_TYPE_EX_HSDPA = "42";
+  static const String MACRO_NET_WORK_TYPE_EX_HSUPA = "43";
+  static const String MACRO_NET_WORK_TYPE_EX_HSPA = "44";
+  static const String MACRO_NET_WORK_TYPE_EX_HSPA_PLUS = "45";
+  static const String MACRO_NET_WORK_TYPE_EX_DC_HSPA_PLUS = "46";
+  static const String MACRO_NET_WORK_TYPE_EX_TD_SCDMA = "61";
+  static const String MACRO_NET_WORK_TYPE_EX_TD_HSDPA = "62";
+  static const String MACRO_NET_WORK_TYPE_EX_TD_HSUPA = "63";
+  static const String MACRO_NET_WORK_TYPE_EX_TD_HSPA = "64";
+  static const String MACRO_NET_WORK_TYPE_EX_TD_HSPA_PLUS = "65";
+  static const String MACRO_NET_WORK_TYPE_EX_802_16E = "81";
+  static const String MACRO_NET_WORK_TYPE_EX_LTE = "101";
+  static const String MACRO_NET_WORK_TYPE_EX_LTE_PLUS = "1011";
+  static const String MACRO_NET_WORK_TYPE_EX_NR = "111";
+}
